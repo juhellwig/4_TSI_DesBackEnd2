91b533eaf1efeda6ebd7681fcd9f1ede24f924be
Juliana <julianahellwig25@gmail.com>
2025-11-10 19:43:10 -0300

    atualizando projeto com construção api

---
+++ projetodbe2/applaravel/app/Http/Controllers/Api/UsuariosController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Resources\UsuarioCollection;
+use App\Http\Resources\UsuarioResource;
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class UsuariosController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new UsuarioCollection(Usuario::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Usuario $usuario)
+    {
+        return new UsuarioResource($usuario);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Usuario $usuario)
+    {
+        //
+    }
+}
