5c92a4f62c05770af7d609dfe14c334b9356b2f4
Juliana <julianahellwig25@gmail.com>
2025-12-21 12:00:53 -0300

    organizando pastas topico12

--- Exercicio2/topico01/poo_php_part2/src/interfaces/IMC.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-namespace Gvg\Dbe2\interfaces;
-
-interface IMC
-{
-	public function calcImc(): void;
-    public function showImc(): void;
-}
--- Exercicio2/topico01/poo_php_part2/src/interfaces/iFuncionario.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Gvg\Dbe2\interfaces;
-
-interface iFuncionario{
-    public function mostrarSalario(): string;
-    public function mostrarTempoContrato(): string;
-    
-}
