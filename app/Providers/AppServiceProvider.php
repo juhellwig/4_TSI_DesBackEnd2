09fcb435bd2bb642ee0d0adcdf2b8ce42ac37727
Juliana <julianahellwig25@gmail.com>
2025-12-21 12:53:01 -0300

    criando arquivos config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,15 @@
-<?php
+<?PHP
 
 namespace App\Providers;
 
+
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+
+//Para erros de RateLimit, limite de requisições para a API
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +26,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //Configura limite de requisições para a API
+        $this->configureRateLimiting();
+         if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+    }
+
+    //Para limitar requisições na API por usuário ou IP
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
